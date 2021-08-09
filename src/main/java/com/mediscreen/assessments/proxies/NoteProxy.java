68d3bbbee684e50547bf56e42be92b57698caef2
DomPOCR <dominiquep.technocenter@gmail.com>
2021-08-09 12:01:11 +0200

    Add spring security

--- src/main/java/com/mediscreen/assessments/proxies/NoteProxy.java
+++ src/main/java/com/mediscreen/assessments/proxies/NoteProxy.java
@@ -1,5 +1,6 @@
 package com.mediscreen.assessments.proxies;
 
+import com.mediscreen.assessments.config.FeignClientConfiguration;
 import com.mediscreen.assessments.dto.Note;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "note", url = "http://note:8082")
+@FeignClient(name = "note", url = "http://note:8082",configuration = FeignClientConfiguration.class)
 public interface NoteProxy {
 
     @GetMapping(value = "/patHistory/patient/{patientId}")
